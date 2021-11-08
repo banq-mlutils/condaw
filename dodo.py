ca19bf3a744a580532050eb64d90adb8ddb6dfb0
johnbanq <johnbanq@gmail.com>
2021-11-08 19:55:40 +0000

    build: complete the build script

--- dodo.py
+++ dodo.py
@@ -1,9 +1,28 @@
+import os
+import shutil
+from pathlib import Path
+
 
 def task_compile():
     """
     compile the condaw module into 1 file
     """
+
+    build_dir = Path("build")
+
+    def mkdir_build():
+        if build_dir.exists():
+            shutil.rmtree(build_dir)
+        build_dir.mkdir()
+
+    def copy():
+        shutil.copy(Path("condaw.bat"), build_dir / "condaw.bat")
+
     return {
-        "actions": ["stickytape condaw/__init__.py --add-python-path . > condaw.py"],
+        "actions": [
+            mkdir_build,
+            copy,
+            "stickytape condaw/__init__.py --add-python-path . > build/condaw"
+        ],
         "verbosity": 2
     }
